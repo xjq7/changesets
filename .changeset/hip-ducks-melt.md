935ec8bff32b34931a38995363991eb2bbaa353a
xiajieqiong <xiajieqiong@tuzhanai.com>
2023-10-27 14:55:13 +0800

    feat: init2

---
+++ .changeset/hip-ducks-melt.md
@@ -0,0 +1,5 @@
+---
+"project1": major
+---
+
+init
--- packages/project1/src/App.jsx
+++ packages/project1/src/App.jsx
@@ -1,6 +1,8 @@
 import { log } from '@gate/utils';
 import { Button } from '@gate/components';
 log(222);
+log(333);
+
 function App() {
   return (
     <div className="App">
